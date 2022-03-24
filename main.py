039c7914631d5a3eb6b15302f0954594f918322d
Zachary Voltz <zvoltz@voltz.us>
2022-03-23 23:56:11 -0500

    Fixed overview, removed references to games, and added a check for valid colors.
    Signed-off-by: Zachary Voltz <zvoltz@voltz.us>

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 """
 Given a file of preferences and a list of names, display the best items according to their preferences
 
-The file is currently an Excel (.xlsx) file with A1 being blank. The rest of the A row is a list of names.
+The file is currently an Excel (.xlsx) file with A1 being blank. The rest of the A row is a list of names of people.
 The first column is a list of items. Their intersections are colored cells: red (FF0000) for disliking the item,
-orange (FF8000) for somewhat disliking it, yellow (FFFF00) for neutral, white/black (FFFFFF/000000) for no opinion,
-lime (08FF00) for somewhat liking it, and green (00FF00) for liking it. Any other colors will cause errors.
+yellow (FFFF00) for neutral, white/black (FFFFFF/000000) for no opinion, and green (00FF00) for liking it.
+Any other colors will cause errors.
 
 TODO:
 1. Add GUI
@@ -28,76 +28,86 @@ colors = {
 # Reads all rows of the given file and puts them in the 2D array rows
 def setup():
     global rows
-    ws = sheet.load_workbook(filename="Games.xlsx").active
+    ws = sheet.load_workbook(filename="example_file.xlsx").active
     rows = list(ws.rows)
 
 
 # Ask the user for all names
 # allows the user to type the same person's name multiple times to increase their influence on the listed items
-def get_players():
-    players = []
-    person = input("Type a player's name: ")
+def get_people():
+    people = []
+    person = input("Type the first person's name: ")
     while person != "":
-        players.append(person)
-        person = input("Type the next player's name or press ENTER to stop: ")
-    return players
+        people.append(person)
+        person = input("Type the next person's name or press ENTER to stop: ")
+    return people
 
 
 def main():
     weight_per_name = get_weight_per_name()
-    num_people = sum(weight_per_name)
-    games = prep_list(num_people)
-    for game in rows:
-        index = 3 * num_people - get_game_score(game[1:], weight_per_name)
-        games[index].append(game[0].value)
-    show_results(games, num_people)
+    max_rating = max(colors.values()) * sum(weight_per_name)
+    items = prep_list(max_rating)
+    for item in rows:
+        index = max_rating - get_rating(item[1:], weight_per_name)
+        items[index].append(item[0].value)
+    show_results(items, max_rating)
 
 
-# Returns a list of size 3 * num_people + 1, each initialized as an empty list
-# 3 * num_people + 1 because max score is 3x the number of people and min is 0.
-def prep_list(num_people):
+# Returns a list of size max_rating + 1, each initialized as an empty list
+# max_rating + 1 because range is 0 to max_rating
+def prep_list(max_rating):
     preference_list = []
-    for rank in range(3 * num_people + 1):
+    for rank in range(max_rating + 1):
         preference_list.append([])
     return preference_list
 
 
 # name weights is how much each person should affect the outcome. It'll be multiplied by their
-# opinion of the game when creating the game scores.
+# opinion of the item when creating the item ratings.
 # Removes the first row of rows because the names of the people are no longer necessary.
 def get_weight_per_name():
-    players = get_players()
+    people = get_people()
     weight_per_name = []
     for name in rows[0]:
-        weight_per_name.append(players.count(name.value))
-    # 1st "Name" is the blank spot for the game names, pop the 1st name
+        weight_per_name.append(people.count(name.value))
+    # A1 is blank so the 1st "Name" is blank, pop to remove
     weight_per_name.pop(0)
-    # names are not part of the games, remove them after accessing them
+    # First row is the list of the names, remove so the rows are just items
     rows.pop(0)
     return weight_per_name
 
 
 # Returns an integer representation of everyone's opinion on that item.
-# Scores are calculated by multiplying their color coded opinion by their name weight.
-def get_game_score(game, weights):
+# Ratings are calculated by multiplying their color coded opinion by their name weight.
+def get_rating(item, weights):
     score = 0
-    for index in range(len(game)):
-        color = game[index].fill.start_color.rgb[2:]
-        score += colors[color] * weights[index]
+    for index in range(len(item)):
+        color = item[index].fill.start_color.index[2:]
+        score += get_color_value(color) * weights[index]
     return score
 
 
-def show_results(games, num_people):
+# Returns an integer representation of a single person's opinion based on the color they put.
+# Checks if the color is valid (see colors dictionary or overview), exits if it is not.
+def get_color_value(color):
+    valid = colors.get(color, None)
+    if valid is None:
+        print("Invalid color in sheet")
+        exit()
+    return valid
+
+
+def show_results(items, max_rating):
     cont = ""
     index = 0
     while cont != "exit":
-        if index >= len(games):
+        if index >= len(items):
             return
-        if not games[index]:
+        if not items[index]:
             index += 1
             continue
-        print(f"With a score of {2 * num_people - index}:")
-        print(games[index])
+        print(f"With a score of {max_rating - index}:")
+        print(items[index])
         cont = input("Press ENTER to see the next best list or type exit to exit: ")
         index += 1
 
