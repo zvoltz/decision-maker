7aa2d2adacb1f3b9399db24654809caeba2ff083
Zachary Voltz <zachary@voltz.us>
2022-10-10 08:48:21 -0500

    Made show_results more readable and clean. Added show_window.
    Signed-off-by: Zachary Voltz <zachary@voltz.us>

--- decide_excel.py
+++ decide_excel.py
@@ -96,31 +96,28 @@ def get_color_value(color):
 # with the highest score and going down. The GUI shows all items with equal rank in the same window, and each new window
 # indicates a single decrease in rank and a single increase in index unless an index has no items to display.
 def show_results(items):
-    cont = ""
     index = 0
-    while cont != "exit":
-        if index >= len(items):
-            return
+    while index < len(items):
         if not items[index]:
             index += 1
             continue
-        popup_text = ", ".join(items[index])
-        layout = [[sg.Multiline(popup_text, s=(150, 2))], [sg.Button("Back"), sg.Button("Next"), sg.Button("Cancel")]]
-        window = sg.Window("Decision Maker", layout)
-        while True:
-            event, values = window.read()
-            if event == sg.WIN_CLOSED or event == 'Cancel':
-                exit()
-            if event == 'Back':
-                index -= 1
-                window.close()
-                break
-            if event == 'Next':
-                index += 1
-                window.close()
-                break
-            else:
-                break
+        index += show_window(", ".join(items[index]))
+
+
+# Given the text to show as string of items, display them as a multiline GUI. Returns either -1 or 1 based on what the
+# user selects. This integer represents whether the integer wants to see the previous index (-1) or next (+1).
+def show_window(text):
+    layout = [[sg.Multiline(text, s=(150, 2))], [sg.Button("Back"), sg.Button("Next"), sg.Button("Cancel")]]
+    window = sg.Window("Decision Maker", layout)
+    event, values = window.read()
+    window.close()
+    match event:
+        case sg.WIN_CLOSED | 'Cancel':
+            exit()
+        case 'Back':
+            return -1
+        case 'Next':
+            return 1
 
 
 def start():
