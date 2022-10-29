dd6173cbade31cf878ad5271b47c5b2ef685015a
Zachary Voltz <zachary@voltz.us>
2022-10-29 11:26:22 -0500

    Fixed comment in main.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 """
-Given a file of preferences and a list of names, display the best items according to their preferences
+Given a file of preferences and a list of names, display the best items according to their preferences.
 
-The file is currently an Excel (.xlsx) file with A1 being blank. The rest of the A row is a list of names of people.
-The first column is a list of items. Their intersections are colored cells: red (FF0000) for disliking the item,
-yellow (FFFF00) for neutral, white/black (FFFFFF/000000) for no opinion, and green (00FF00) for liking it.
-Any other colors will cause errors.
+The program can currently use both Excel (.xlsx) and database (.db) files.
+
+An Excel (.xlsx) file should start with A1 being blank. The rest of the A row is a list of names. The first column is a
+list of items. Their intersections are colored cells: red (FF0000) for disliking the item, yellow (FFFF00) for neutral,
+white/black (FFFFFF/000000) for no opinion, and green (00FF00) for liking it. Any other colors will be counted as no
+opinion.
+
+A database file should be made using the excel_to_SQL file, but if you want to make your own, the first column should be
+named "object_name" and type text. Each subsequent column should be the name of each person and type smallint(1). Each
+row consists of the name of an item and each person's preference of the item in the form of a number.
 
 TODO:
-1. Add GUI.
-2. Remove reference to games (generalize to work for all preferences like restaurants).
-3. Implement support for comments about each person's preference.
-4. Add support for android.
-5. Update README.
-6. Add way to make a new database from scratch.
+1. Implement support for comments about each person's preference.
+2. Add support for android.
+3. Add way to make a new database from scratch.
+4. Add web support.
 """
 
 
